f4f1441f21361fbada0c8f33ee60a43ae9fe8935
ReMainyu <angrymango.dev@gmail.com>
2024-03-02 22:38:32 +0800

    Added moveToPOI and force exit

--- assist.py
+++ assist.py
@@ -1,11 +1,24 @@
+#!/usr/bin/env python
+__author__      = "ReMainyu"
+
+import time
+import threading
+from enum import Enum
+from random import randint
+
 import pydirectinput
 from pynput import keyboard
 from pynput.keyboard import Listener
-import time
-import threading
 
 is_active = False
 pydirectinput.FAILSAFE = False
+
+class POI(Enum):
+    QUEST_COUNTER = 0
+    BLACKSMITH = 1
+    SIERO = 2
+    ZATHBA = 3
+    DOCKS = 4
 
 def on_press(key):
     global is_active
@@ -21,7 +34,9 @@ def on_press(key):
                 thread = threading.Thread(target=basicSlimeBlasting)
                 thread.start()
         case keyboard.Key.f3:
-            moveToQuestCounter()
+            moveToPOI(0)
+        case keyboard.Key.f5:
+            exit()
     
 def autofire():
     '''Press and hold left click in accordance to Rackam's perfect primary attack timing'''
@@ -30,23 +45,43 @@ def autofire():
         time.sleep(0.55)
         pydirectinput.mouseUp()
 
-def pickLocation():
-    pass
-
-def moveToQuestCounter():
+def moveToPOI(order):
     pydirectinput.press("r")
     pydirectinput.press("enter")
+    for i in range(order):
+        pydirectinput.press("down")
     pydirectinput.press("enter")
+
     pydirectinput.keyDown("w")
-    time.sleep(1.1)
+    time.sleep(1.4)
     pydirectinput.keyUp("w")
     pydirectinput.press("f")
 
 def basicSlimeBlasting():
-    '''Moves into postion for slime blasting'''
-    pydirectinput.keyDown("w")
-    time.sleep(6.25)
-    pydirectinput.keyUp("w")
+    '''Selects and move into postion for slime blasting'''
+    location = randint(0, 3)
+    print(location)
+    match location:
+        case 1:
+            pydirectinput.keyDown("w")
+            time.sleep(6.25)
+            pydirectinput.keyUp("w")
+        case 2:
+            pydirectinput.keyDown("w")
+            time.sleep(2.8)
+            pydirectinput.keyDown("a")
+            time.sleep(2.1)
+            pydirectinput.keyUp("w")
+            pydirectinput.keyUp("a")
+        case 3:
+            pydirectinput.keyDown("w")
+            time.sleep(2.8)
+            pydirectinput.keyDown("d")
+            time.sleep(2.1)
+            pydirectinput.keyUp("w")
+            pydirectinput.keyUp("d")
+        case _:
+            pass
 
     autofire()
 
