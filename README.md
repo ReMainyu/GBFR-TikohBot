30cebffc699290cdaa15c0831e783a5861b6f566
ReMainyu <angrymango.dev@gmail.com>
2024-03-11 19:08:39 +0800

    Added: Full auto mode

--- README.md
+++ README.md
@@ -1,33 +1,23 @@
 # GBFR Grinding Assistant
 
-This script is designed to alleviate the repetitive aspect of GBFR. It supports automation and shortcuts to enhance the experience and to eliminate Carpal Tunnel Syndrome.
+This script is designed to alleviate the repetitive aspect of GBFR using Python. It supports automation and shortcuts to enhance the experience and to eliminate Carpal Tunnel Syndrome. I'm aware that AHK exists, however I have Python installed and not AHK. Furthermore, I wanted to implement QoL for offline play.
 
 ## Features
 
 - Automates perfect Rackam's shots in any quest, but works best in Slimepede.
-- Shortcut for using fast travel to interact with NPCs and for starting Slimepede.
+- Using fast travel to interact with NPCs and for starting Slimepede.
+- Moving into position in Slimepede and begin automatic fire.
+- Full auto mode:
+  - Travels to the Quest Counter, select and start the Slimepede mission. Moves into the back upon mission entry and begin automatic fire.
+  - Will continue to move into position and automatic fire provided repeat is turned on.
 - Automates transmutation and transmarvel.
 - Reduces chest opening timer and quest result timer.
-- Start or stop quest from repeating indefinitely.
+- Repeat quest indefinitely.
 
 ## Disclaimer
 
-Script has been tested in closed party and offline mode.
-Timer for macros are tested on 60 FPS, and will have a different result at 30/120 FPS.
-
-## Controls
-
-- F1: Activates automatic fire for Rackam.
-- F2: Moves to one of the four designated positions before activating automatic fire.
-- F3: Selects and starts Slimepede quest.
-- F4: Simply mashes the enter key. Be sure to select the right transmutation level or transmarvel before activating F4.
-- F6: Turns chest opening timer to 0s and quest result page to 5s.
-- F7: Resets the quest repeat counter.
-- r + 1: Uses fast travel to move and interact with the Quest Counter and talk to the npc.
-- r + 2: Uses fast travel to move and interact with the Blacksmith.
-- r + 3: Uses fast travel to move interact with Sierokarte.
-- r + 4: Uses fast travel to move interact with Zathba.
-- Ctrl + q: Force quits the script.
+Script has been tested in closed matchmaking and offline mode.
+Timer for macros are tested on 60FPS, and will have a different result at 30FPS or 120FPS.
 
 ## Setup
 
@@ -36,5 +26,24 @@ Open up the command prompt(cmd) and run the following commands to install the de
 ```sh
 pip install -r requirements.txt
 ```
+__Always__ run GBFR first before the script.
+Go to Game Options > Gameplay.
+- Ensure "Quest Cutscene Auto-Skip" is set to On and "Loading Screen Skip" is set to Auto Skip.
+
+See Controls.
+
+## Controls
+
+- F1: Activates automatic fire for Rackam.
+- F2: Moves to one of the four designated positions before activating automatic fire.
+- F3: Travels to the Quest Counter, select and start the Slimepede mission. Moves into the back upon mission entry and begins Automatic fire.
+- F4: Simply mashes the enter key. Be sure to select the right transmutation level or transmarvel before activating F4.
+- F5: Turns chest opening timer to 0s and quest result page to 5s.
+- F6: Resets the quest repeat counter.
+- r + 1: Uses fast travel to move and interact with the Quest Counter and talk to the NPC.
+- r + 2: Uses fast travel to move and interact with the Blacksmith.
+- r + 3: Uses fast travel to move interact with Sierokarte.
+- r + 4: Uses fast travel to move interact with Zathba.
+- Ctrl + q: Force quits the script.
 
 [Python]: <https://www.python.org/downloads>
--- assist.py
+++ assist.py
@@ -14,6 +14,11 @@ from pynput.keyboard import Key, HotKey, Listener
 pydirectinput.FAILSAFE = False
 pydirectinput.PAUSE = 0.05
 
+QUEST_TIMER_OFFSETS = [0x1D0, 0x34]
+CHEST_TIMER_OFFSETS = [0x1D0, 0x4F4]
+RESULT_TIMER_OFFSETS = [0x2C8, 0x60, 0x70, 0x0, 0x88]
+REPEAT_COUNTER_OFFSETS = [0x4A0]
+
 class TOWN_SHORTCUTS(Enum):
     QUEST_COUNTER = 0
     BLACKSMITH = 1
@@ -23,7 +28,11 @@ class TOWN_SHORTCUTS(Enum):
 
 class Assist:
     def __init__(self):
-        self.mem = pymem.Pymem('granblue_fantasy_relink.exe')
+        try:
+            self.mem = pymem.Pymem('granblue_fantasy_relink.exe')
+        except:
+            exit()
+        
         self.is_active = False
         self.enable_skip = False
         self.enable_quest_repeat = False
@@ -41,22 +50,23 @@ class Assist:
             time.sleep(0.6)
             pydirectinput.mouseUp()
 
-    def basic_slimeblast(self):
+    def basic_slimeblast(self, location=None):
         '''Selects and move into postion for slime blasting'''
-        location = randint(0, 3)
+        if location == None:
+            location = randint(0, 3)
         match location:
-            case 1:
+            case 0:
                 pydirectinput.keyDown('w')
                 time.sleep(6.25)
                 pydirectinput.keyUp('w')
-            case 2:
+            case 1:
                 pydirectinput.keyDown('w')
                 time.sleep(2.9)
                 pydirectinput.keyDown('a')
                 time.sleep(2.2)
                 pydirectinput.keyUp('w')
                 pydirectinput.keyUp('a')
-            case 3:
+            case 2:
                 pydirectinput.keyDown('w')
                 time.sleep(2.9)
                 pydirectinput.keyDown('d')
@@ -67,7 +77,7 @@ class Assist:
                 pass
 
         self.autofire()
-        
+    
     def move_to_POI(self, repeat):
         pydirectinput.press('r')
         pydirectinput.press('enter')
@@ -78,11 +88,8 @@ class Assist:
         pydirectinput.keyUp('w')
         pydirectinput.press('f')
 
-    def auto_transmute(self):
-        while self.is_active:
-            pydirectinput.press('enter')
-    
     def queue_slimepede(self):
+        pydirectinput.press('enter')
         pydirectinput.press('e')
         pydirectinput.press('up')
         pydirectinput.press('enter')
@@ -94,7 +101,28 @@ class Assist:
         pydirectinput.press('3')
         pydirectinput.press('enter')
 
-    def get_pointer_address(self,base, offsets):
+    def auto_transmute(self):
+        while self.is_active:
+            pydirectinput.press('enter')
+    
+    def full_auto_slimepede(self):
+        self.move_to_POI(0)
+        time.sleep(1.5)
+        self.queue_slimepede(0)
+
+        while self.is_active:
+            try:
+                chest_timer_ptr_value = self.mem.read_float(self.get_pointer_address(self.mem.base_address + 0x05CEC108, QUEST_TIMER_OFFSETS))
+                if chest_timer_ptr_value > 178 and chest_timer_ptr_value < 180:
+                    threading.Thread(target=self.basic_slimeblast).start()
+                    time.sleep(180)
+                    self.is_active = False
+                    time.sleep(33)
+                    self.is_active = True
+            except:
+                pass
+
+    def get_pointer_address(self, base, offsets):
         remote_pointer = RemotePointer(self.mem.process_handle, base)
         for offset in offsets:
             if offset != offsets[-1]:
@@ -104,21 +132,17 @@ class Assist:
 
     def skip_rewards(self):
         while self.enable_skip:
-            chest_timer_offsets = [0x1D0, 0x4F4]
-            result_timer_offsets = [0x2C8, 0x60, 0x70, 0x0, 0x88]
-            repeat_counter_offsets = [0x4A0]
-
             try:
-                chest_timer_ptr_value = self.mem.read_float(self.get_pointer_address(self.mem.base_address + 0x05CEC108, chest_timer_offsets))
-                result_timer_ptr_value = self.mem.read_float(self.get_pointer_address(self.mem.base_address + 0x067323B8, result_timer_offsets))
+                chest_timer_ptr_value = self.mem.read_float(self.get_pointer_address(self.mem.base_address + 0x05CEC108, CHEST_TIMER_OFFSETS))
+                result_timer_ptr_value = self.mem.read_float(self.get_pointer_address(self.mem.base_address + 0x067323B8, RESULT_TIMER_OFFSETS))
 
                 if chest_timer_ptr_value > 10:
-                    self.mem.write_float(self.get_pointer_address(self.mem.base_address + 0x05CEC108, chest_timer_offsets), 0.0)
+                    self.mem.write_float(self.get_pointer_address(self.mem.base_address + 0x05CEC108, CHEST_TIMER_OFFSETS), 0.0)
                     if self.enable_quest_repeat:
-                        self.mem.write_int(self.get_pointer_address(self.mem.base_address + 0x06772160, repeat_counter_offsets), 8)
+                        self.mem.write_int(self.get_pointer_address(self.mem.base_address + 0x06772160, REPEAT_COUNTER_OFFSETS), 8)
                 
-                if result_timer_ptr_value > 28 and result_timer_ptr_value < 30:
-                    self.mem.write_float(self.get_pointer_address(self.mem.base_address + 0x067323B8, result_timer_offsets), 5.0)
+                if result_timer_ptr_value > 28 and result_timer_ptr_value < 35:
+                    self.mem.write_float(self.get_pointer_address(self.mem.base_address + 0x067323B8, RESULT_TIMER_OFFSETS), 5.0)
             except:
                 pass
 
@@ -137,19 +161,19 @@ class Assist:
             case Key.f3:
                 self.is_active = not self.is_active
                 if self.is_active:
-                    thread = threading.Thread(target=self.queue_slimepede)
+                    thread = threading.Thread(target=self.full_auto_slimepede)
                     thread.start()
             case Key.f4:
                 self.is_active = not self.is_active
                 if self.is_active:
                     thread = threading.Thread(target=self.auto_transmute)
                     thread.start()
-            case Key.f6:
+            case Key.f5:
                 self.enable_skip = not self.enable_skip
                 if self.enable_skip:
                     thread = threading.Thread(target=self.skip_rewards)
                     thread.start()
-            case Key.f7:
+            case Key.f6:
                 self.enable_quest_repeat = not self.enable_quest_repeat
             case _:
                 for hotkey in self.HOTKEYS:
